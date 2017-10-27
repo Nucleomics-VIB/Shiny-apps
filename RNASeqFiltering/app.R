915da13f64c82b93806330ad9fa6a3fa192f2588
Stephane Plaisance <stephane.plaisance@vib.be>
2017-10-27 16:28:09 +0200

    edits

--- RNASeqFiltering/app.R
+++ RNASeqFiltering/app.R
@@ -23,7 +23,7 @@ script.version <- "1.0"
 #defaults
 def.min.lfc <- 0
 def.max.pv <- 1
-contrast.names <- NULL
+contrast.names.all <- NULL
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
@@ -53,13 +53,16 @@ ui <- fluidPage(
       tags$hr(),
       tipify(fileInput('file1', 'Choose a StatisticalResults XLSX File', accept='.xlsx'),
              "the Data is a MS-Excel file provided by the Nucleomics Core, with a single worksheet reporting pairwise group comparison results (with logFC and FDR columns for each contrast), you may produce a compatible file based on the test data provided here."),
-      tipify(uiOutput("choose_contrasts"),"Use the checkboxes to define which contrasts should be filtered to produce the result table (all by default)"),
       tags$h4("modify cutoffs and click ", tags$em("Filter")),
+      tipify(uiOutput("choose_contrasts"),"Use the checkboxes to define which contrasts should be filtered to produce the result table (all by default)"),
+      tipify(textInput('min.lfc', "logFC cutoff", value = def.min.lfc, width = "150px"),"provide the absolute value of the logFC cutoff. All genes whose logFC is < -(value) or > +(value) will be kept"),
+      tipify(textInput('max.pv', "corPvalue limit", value = def.max.pv, width = "150px"),"provide the max acepted FDR (corrected Pvalue) to keep a gene"),
+      tipify(radioButtons('stringency', "Filter stringency:", c("one_or_more", "all_contrasts"), selected="one_or_more"),"the filtering should apply to at least one of the contrasts or to all selected contrasts (default to at least one)"),
       actionButton(inputId='goButton', "Filter", style='padding:4px; font-weight: bold; font-size:150%'),
-      tipify(textInput('min.lfc', "logFC cutoff", value = def.min.lfc, width = "100px"),"provide the absolute value of the logFC cutoff. All genes whose logFC is < -(value) or > +(value) will be kept"),
-      tipify(textInput('max.pv', "corPvalue limit", value = def.max.pv, width = "100px"),"provide the max acepted FDR (corrected Pvalue) to keep a gene"),
+      tags$hr(),
+      tags$a(href="javascript:history.go(0)", tags$i("reset page content"), alt="Reset page"),
       tipify(downloadButton('downloadTable', 'Filtered Table'),"Download the filtered Statistical Table subset"),
-      tipify(downloadButton('downloadList', 'Filtered ID-list'),"Download the filtered ENS geneID list for use in fpkm2heatmap or any gene list aware tool"),
+      tipify(downloadButton('downloadIDList', 'Filtered ID-list'),"Download the filtered ENS geneID list for use in fpkm2heatmap or any gene list aware tool"),
       tipify(downloadButton('downloadMM', 'M&M'),"Download a text including the names and versions of all packages used in this webtool"),
       width=2
     ),
@@ -70,9 +73,10 @@ ui <- fluidPage(
       textOutput("contrasts"),
       textOutput('min.lfc'),
       textOutput('max.pv'),
+      textOutput('stringency'),
       br(),
-      DT::dataTableOutput("filt.data.table"),
-      textOutput('filt.data.cnt')
+      textOutput('filt.data.cnt'),
+      DT::dataTableOutput("filt.data.table")
     )
   # end sidebarLayout
   )
@@ -91,26 +95,29 @@ server <- function(input, output) {
   load.data <- reactive({
     inFile <- input$file1
     if (is.null(inFile)) return("Waiting for data")
+
     # load data from excel file
     dat <- read.xlsx(inFile$datapath, sheet=1)
+
     # count contasts
     chr.col <- which(colnames(dat)==as.vector("Chromosome"))
-    # store contrast names from LogFC column names
-    contrast.names <- gsub(".:.logFC", "", colnames(dat)[seq(3, chr.col-1, 5)])
 
-    # use values to populate the UI contrasts list
+    # store contrast names from LogFC column names
+    contrast.names.all <- gsub(".:.logFC", "", colnames(dat)[seq(3, chr.col-1, 5)])
+
+    # use the obtained values to populate the UI contrasts list
     output$choose_contrasts <- renderUI({
       # Create the checkboxes and select them all by default
-      checkboxGroupInput("contrasts", "Choose contrasts",
-                         choices  = contrast.names,
-                         selected = contrast.names)
+      checkboxGroupInput("contrasts", "Check contrasts to be filtered",
+                         choices  = contrast.names.all,
+                         selected = contrast.names.all)
     })
 
     # reduce data size and keep only filterable columns
-    keep.data <- dat[,c(2, 1, sort(c(seq(3, chr.col-1, 5), seq(5, chr.col-1, 5))), chr.col:ncol(dat))]
+    data <- dat[,c(2, 1, sort(c(seq(3, chr.col-1, 5), seq(5, chr.col-1, 5))), chr.col:ncol(dat))]
 
     # return data as 'load.data()'
-    keep.data
+    data
   })
 
   output$full.data.cnt <- reactive({
@@ -132,13 +139,59 @@ server <- function(input, output) {
     paste("max corrected-Pvalue: ", input$max.pv)
   })
 
+  output$stringency <- renderText({
+    paste("stringency: ", input$stringency)
+  })
+
+  output$keepunfiltered <- renderText({
+    paste("Action for unfiltered columns is set to : ", input$keepunfiltered)
+  })
+
   filtered.data <- eventReactive({input$goButton}, {
     # do nothing in absence of data
     if (is.null(load.data())) return(NULL)
+    filtered <- load.data()
 
-    # filter data TBD
-    filtered_data <- load.data()
-  })
+    # filter values in input$contrasts selected LR and pval columns
+    #logfc.cols <- grepl( ".:.logFC" , names( filtered ) )
+    #pval.cols <- grepl( ".:.FDR" , names( filtered ) )
+    logfc.cols <- grepl( paste(paste(input$contrasts, ".:.logFC", sep=""), collapse="|") , names( filtered ) )
+    pval.cols <- grepl( paste(paste(input$contrasts, ".:.FDR", sep=""), collapse="|") , names( filtered ) )
+
+    # filter depending on input$stringency
+    if (input$stringency=="one_or_more") {
+      # handle case where only one contrast remains
+      if (length(input$contrasts)>1) {
+        filtered <- filtered[apply(abs(filtered[,logfc.cols]), 1, max) >= input$min.lfc,]
+        filtered <- filtered[apply(filtered[,pval.cols], 1, min) <= input$max.pv,]
+      } else {
+        filtered <- filtered[abs(filtered[,logfc.cols]) >= input$min.lfc,]
+        filtered <- filtered[filtered[,pval.cols] <= input$max.pv,]
+      }
+    } else {
+      # input$stringency=="all_contrasts"
+      # handle case where only one contrast remains
+      if (length(input$contrasts)>1) {
+        filtered <- filtered[apply(abs(filtered[,logfc.cols]), 1, min) >= input$min.lfc,]
+        filtered <- filtered[apply(filtered[,pval.cols], 1, max) <= input$max.pv,]
+      } else {
+        filtered <- filtered[abs(filtered[,logfc.cols]) >= input$min.lfc,]
+        filtered <- filtered[filtered[,pval.cols] <= input$max.pv,]
+      }
+    }
+
+    # sort in increasing Pvalue order across all present contrasts
+    # look at minimum pval in each row across all pval columns and sort rows based on these value
+    # (Merci Rekin's)
+    if (length(input$contrasts)>1) {
+      filtered.sorted <- filtered[order(apply(filtered[,pval.cols], 1, min)),]
+    } else {
+      filtered.sorted <- filtered[order(filtered[,pval.cols]),]
+    }
+
+    # return final results
+    filtered.sorted
+    })
 
   output$filt.data.cnt <- reactive({
     if (is.null(filtered.data())) return(NULL)
@@ -152,7 +205,7 @@ server <- function(input, output) {
 
   output$downloadTable <- downloadHandler(
     filename = function() {
-      paste(input$outfile, "_filtered-data.xlsx", sep="")
+      paste(input$file1, "_filtered-data.xlsx", sep="")
     },
     content = function(file) {
       hs <- createStyle(textDecoration = "BOLD", fontColour = "#FFFFFF", fontSize=12,
@@ -169,9 +222,18 @@ server <- function(input, output) {
     }
   )
 
+  output$downloadIDList <- downloadHandler(
+    filename = function() {
+      paste(input$file1, "_filteredID-list.txt", sep="")
+    },
+    content = function(file) {
+      write(filtered.data()[,2], file = file, ncolumns = 1, sep = "\t")
+    }
+  )
+
   output$downloadMM <- downloadHandler(
     filename = function() {
-      paste(app.name, "_session_info.txt", sep="")
+      print(capture.output(paste(app.name, "_session_info.txt", sep="")))
     },
     content = function(file) {
       sink(file, append=TRUE)
