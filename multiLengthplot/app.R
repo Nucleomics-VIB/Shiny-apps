af7c7de5cf4f8072678cc52ce65bab962a6c3945
Stephane Plaisance <stephane.plaisance@vib.be>
2018-04-11 10:09:06 +0200

    edits

--- multiLengthplot/app.R
+++ multiLengthplot/app.R
@@ -7,7 +7,7 @@ library("scales")
 library("DT")
 
 # you may uncomment the next line to allow large input files (4GB)
-options(shiny.maxRequestSize=1000*1024^2)
+options(shiny.maxRequestSize=4*1000*1024^2)
 # the following test checks if we are running on shinnyapps.io to limit file size dynamically
 # ref: https://stackoverflow.com/questions/31423144/how-to-know-if-the-app-is-running-at-local-or-on-server-r-shiny/31425801#31425801
 #if ( Sys.getenv('SHINY_PORT') == "" ) { options(shiny.maxRequestSize=1000*1024^2) }
